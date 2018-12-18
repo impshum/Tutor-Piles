d8603ab366a0471cb1e2bb09c9b015bd5c52f74e
impshum <impshum@sausage.local>
2018-12-18 20:03:22 +0000

    moar

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -9,18 +9,85 @@ $(document).ready(function() {
       console.log(leaver);
       $(leaver).find('.move').addClass('animated fadeInUp')
 
-      $('.page-count span').text(nextIndex);
+      $('.pager').text(nextIndex);
     },
     afterRender: function() {
       //$('#callbacksDiv').append('<p>afterRender</p>');
     },
     afterLoad: function(anchorLink, index) {
-      $('.page-count span').text(index);
+      $('.pager').text(index);
     }
   });
   $('pre code').each(function(i, block) {
-   hljs.highlightBlock(block);
- });
- //$('#section1').find('.move').addClass('animated fadeInUp')
-  $('body').removeClass('fade-out');
+    hljs.highlightBlock(block);
+  });
+
+
+
+  $('a').each(function() {
+    if (location.hostname === this.hostname || !this.hostname.length) {
+
+      var link = $(this).attr("href");
+
+      if (link.match("^#")) {
+
+        $(this).click(function() {
+          var target = $(link);
+          target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+          if (target.length) {
+            $('html,body').animate({
+              scrollTop: target.offset().top - 70
+            }, 1000);
+            return false;
+          }
+        });
+
+      } else if (link.match("^mailto")) {} else {
+
+        $(this).click(function(e) {
+          e.preventDefault();
+          $('html').addClass('fadeSiteOut');
+          setTimeout(function() {
+            window.location = link;
+          }, 1000);
+        });
+
+      }
+
+    }
+  });
+
+  clockUpdate();
+  setInterval(clockUpdate, 1000);
+
+  $('.paged').text($('.section').length);
+
 });
+
+function clockUpdate() {
+  var date = new Date();
+
+  function addZero(x) {
+    if (x < 10) {
+      return (x = "0" + x);
+    } else {
+      return x;
+    }
+  }
+
+  function twelveHour(x) {
+    if (x > 24) {
+      return (x = x - 24);
+    } else if (x == 0) {
+      return (x = 24);
+    } else {
+      return x;
+    }
+  }
+
+  var h = addZero(twelveHour(date.getHours()));
+  var m = addZero(date.getMinutes());
+  var s = addZero(date.getSeconds());
+
+  $(".digital-clock").text(h + ":" + m + ":" + s);
+}
--- index.php
+++ index.php
@@ -23,22 +23,39 @@ function read_file($md_file)
 
 function all($Parsedown)
 {
-    $dir = 'content/';
-    $files = glob($dir . '*.md', GLOB_BRACE);
-    $c = 0;
-
-    foreach ($files as $file) {
-				$c++;
-        $col = 'section' . $c;
-        $content = read_file($file, $Parsedown);
-
-        echo "<div class='section' id='$col'><div class='container'><div class='columns is-vcentered is-mobile is-centered'>";
-        foreach ($content as $half) {
-          echo "<div class='column is-6 move'><div class='content has-text-white has-text-left'>";
-          echo $Parsedown->text($half);
-          echo "</div></div>";
+    if (isset($_REQUEST['p'])) {
+        $dir = 'content/'. $_REQUEST['p'] . '/';
+        $files = glob($dir . '*.md', GLOB_BRACE);
+        $c = 0;
+        foreach ($files as $file) {
+            $c++;
+            $col = 'section' . $c;
+            $content = read_file($file, $Parsedown);
+            echo "<div class='section' id='$col'><div class='container'><div class='columns is-vcentered is-centered'>";
+            foreach ($content as $half) {
+                echo "<div class='column is-6 move'><div class='content has-text-white has-text-left'>";
+                echo $Parsedown->text($half);
+                echo "</div></div>";
+            }
+            echo "</div></div></div>";
         }
-        echo "</div></div></div>";
+    } else {
+        $content = read_file('content/index.md', $Parsedown);
+        echo "<style>#pp-nav, .page-count {display:none;}</style><div class='section'><div class='container'><div class='columns is-vcentered is-centered'><div class='content'><h1 class='is-size-1'>TUTOR PILES</h1></div></div></div></div>";
+    }
+}
+
+function get_dirs()
+{
+    $dirs = glob('content/*', GLOB_ONLYDIR);
+		natsort($dirs);
+		foreach ($dirs as $dir) {
+        $d1 = explode('/', $dir);
+        $d2 = strstr($d1[1], '-');
+        $d3 = str_replace('-', ' ', $d2);
+        $d4 = ltrim($d3);
+        $d5 = ucfirst($d4);
+        echo "<li><a href='?p=$d1[1]'>$d5</a></li>";
     }
 }
 ?>
@@ -56,16 +73,26 @@ function all($Parsedown)
 	<link rel="stylesheet" href="assets/css/styles.css">
 	<link rel="stylesheet" href="assets/css/ani.css">
 	<link rel="stylesheet" href="assets/css/atom-one-dark.min.css">
-	<!--link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/highlight.js/9.13.1/styles/monokai-sublime.min.css"-->
 </head>
 
 <body>
 
 	<div id="pagepiling">
-		<?php all($Parsedown) ?>
+		<?php all($Parsedown); ?>
 	</div>
 
-	<div class="page-count animated fadeInLeft delay-4s"><span>1</span>/5</div>
+	<div class="page-count animated fadeInLeft delay-2s"><span class="pager">1</span>/<span class="paged">1</span></div>
+	<div class="menu-trigger">
+		<a class="home" href='index.php'><img src="assets/img/home.png"></a>
+		<aside class="menu">
+		  <p class="menu-label">Slides</p>
+		  <ul class="menu-list">
+				<?php get_dirs(); ?>
+		  </ul>
+		</aside>
+	</div>
+
+	<div class="digital-clock animated fadeInRight delay-2s">00:00:00</div>
 
 	<script src="assets/js/jquery-3.3.1.min.js"></script>
 	<script src="assets/js/pagepiling.min.js"></script>
